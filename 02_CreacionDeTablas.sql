4adc7edc5ef4960f9e30b4c58a856004e0afd05e
Adr1z <97919020+Adr1z@users.noreply.github.com>
2025-10-31 01:15:27 -0300

    Refactor SQL table creation statements

--- 02_CreacionDeTablas.sql
+++ 02_CreacionDeTablas.sql
@@ -1,49 +1,49 @@
 CREATE TABLE Administracion(
-	 id_Administracion int PRIMARY KEY,
-	 nombre varchar(100) NOT NULL,
-	correo_electronico varchar(20) NOT NULL
+	 ID_Administracion INT PRIMARY KEY,
+	 Nombre VARCHAR(100) NOT NULL,
+	Correo_Electronico VARCHAR(20) NOT NULL
 )
 
 CREATE TABLE Consorcio(
-	id_Consorcio int PRIMARY KEY,
-	id_administracion int FOREIGN KEY References  Administracion(id_Administracion) ON DELETE CASCADE,
-	Nombre varchar(20) NOT NULL,
-	Direccion varchar(20) NOT NULL,
+	ID_Consorcio INT PRIMARY KEY,
+	ID_Administracion INT FOREIGN KEY References  Administracion(ID_Administracion) ON DELETE CASCADE,
+	Nombre VARCHAR(20) NOT NULL,
+	Direccion VARCHAR(20) NOT NULL,
 	Superficie DECIMAL(10,2) NOT NULL
 )
 
 CREATE TABLE unidad_Funcional(
-	ID_UF int PRIMARY KEY,
-	ID_Consorcio int FOREIGN KEY references Consorcio(id_Consorcio) ON DELETE CASCADE,
-	piso char(2) NOT NULL, check (piso = 'PB' or (TRY_CAST(piso as INT) between 0 and 20)),
-	departamento CHAR(1) NOT NULL,
+	ID_UF INT PRIMARY KEY,
+	ID_Consorcio INT FOREIGN KEY references Consorcio(ID_Consorcio) ON DELETE CASCADE,
+	Piso CHAR(2) NOT NULL, check (Piso = 'PB' or (TRY_CAST(Piso as INT) between 0 and 20)),
+	Departamento CHAR(1) NOT NULL,
 	Superficie_m2 DECIMAL(10,2) NOT NULL,check (Superficie_m2>10),
-	porcentaje_prorrateo DECIMAL(10,2) NOT NULL, check( porcentaje_prorrateo >=0 and porcentaje_prorrateo <= 100),
-	tiene_cochera CHAR(2) NOT NULL, check (tiene_cochera in ('SI','NO')),
-	tiene_bahulera CHAR(2) NOT NULL, check (tiene_bahulera in ('SI','NO'))
+	Porcentaje_Prorrateo DECIMAL(10,2) NOT NULL, check( Porcentaje_Prorrateo >=0 and Porcentaje_Prorrateo <= 100),
+	Tiene_Cochera CHAR(2) NOT NULL, check (Tiene_Cochera in ('SI','NO')),
+	Tiene_Bahulera CHAR(2) NOT NULL, check (Tiene_Bahulera in ('SI','NO'))
 )
 
 CREATE TABLE Persona(
-	tipo_documento int,
-	numero_documento int,
-	nombre varchar(20) NOT NULL,
-	apellido varchar(20) NOT NULL,
-	correo_electronico varchar(35) NOT NULL,
-	Telefono int NOT NULL CHECK(Telefono between 1100000000 and 1200000000),
-	PRIMARY KEY(tipo_documento,numero_documento)
+	Tipo_Documento INT,
+	Numero_documento INT,
+	Nombre VARCHAR(20) NOT NULL,
+	Apellido VARCHAR(20) NOT NULL,
+	Correo_Electronico VARCHAR(35) NOT NULL,
+	Telefono BIG INT NOT NULL CHECK(Telefono between 1100000000 and 1200000000),
+	PRIMARY KEY(Tipo_Documento,Numero_documento)
 )
 
 CREATE TABLE Relacion_UF_Persona(
-	id_relacion int Primary key,
-	ID_UF int,
-	tipo_documento int,
-	num_documento int,
-	fecha_inicio DATE NOT NULL,
-	fecha_fin DATE NOT NULL, check(fecha_fin > fecha_inicio),
-	rol varchar(20) NOT NULL,
-	CBU_CVU_Pago char(22) NOT NULL,
-	CONSTRAINT FK_Relacion_Persona FOREIGN KEY (tipo_documento, num_documento)
-    REFERENCES Persona(tipo_documento, numero_documento) ON DELETE CASCADE,
+	ID_Relacion INT Primary key,
+	ID_UF INT,
+	Tipo_Documento INT,
+	Num_Documento INT,
+	Fecha_Inicio DATE NOT NULL,
+	Fecha_Fin DATE NOT NULL, check(Fecha_Fin > Fecha_Inicio),
+	Rol VARCHAR(20) NOT NULL,
+	CBU_CVU_Pago CHAR(22) NOT NULL,
+	CONSTRAINT FK_Relacion_Persona FOREIGN KEY (Tipo_Documento, Num_Documento)
+    REFERENCES Persona(Tipo_Documento, Numero_documento) ON DELETE CASCADE,
 	CONSTRAINT FK_Relacion_UF FOREIGN KEY (ID_UF)
     REFERENCES Unidad_Funcional(ID_UF) ON DELETE CASCADE
 
@@ -112,3 +112,4 @@ CREATE TABLE Pago (
         REFERENCES Detalle_Expensa(ID_Detalle)
         ON DELETE CASCADE
 )
+
